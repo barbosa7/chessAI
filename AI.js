7032ccb590eb71105501573c58c1057bf949b3f1
tomasbarbosa <tomasgiaossbarbosa@gmail.com>
2022-06-07 23:27:30 +0200

    t push

---
+++ AI.js
@@ -0,0 +1,10 @@
+import { Chess } from 'chess.js'
+
+const chess = new Chess()
+
+while (!chess.game_over()) {
+    const moves = chess.moves()
+    const move = moves[Math.floor(Math.random() * moves.length)]
+    chess.move(move)
+}
+console.log(chess.pgn())
